f276a8c594e280d7c28ea9f502b7ce79340cf4e3
mikedr <derosamiguel@gmail.com>
2022-02-06 21:11:45 -0300

    Agregamos Swagger

--- src/main/java/com/example/aws/HelloSpringBootApplication.java
+++ src/main/java/com/example/aws/HelloSpringBootApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @RestController
+@EnableWebMvc
 public class HelloSpringBootApplication {
 
 	public static void main(String[] args) {
